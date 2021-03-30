3e306909fea36ac49ce680cf2ea26e8f8564e473
Vinicius de Santana <vinicius.vts@gmail.com>
2021-03-30 12:33:45 -0300

    fix: add colons because spaces are considered

--- views/template/custom-fields.php
+++ views/template/custom-fields.php
@@ -61,7 +61,7 @@ if (isset($fields->errors)) {
 				<?php
 				if (isset($field->validation_rules->valid_options)){
 					foreach($field->validation_rules->valid_options as $key => $option){
-						echo ($key + 1).') '.$option->value;
+						echo ($key + 1).') "'.$option->value.'"';
 						echo '<br>';
 					}
 				} else {
